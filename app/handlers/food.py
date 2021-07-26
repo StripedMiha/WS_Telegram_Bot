e17133117f4d1349bf10ebc3d4810aa9ae4f9273
Mikhail Ignatenko <Mixa418@gmail.com>
2021-07-26 09:48:52 +0300

    add /cancel in /food

--- app/handlers/food.py
+++ app/handlers/food.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from app.handlers.common import cmd_cancel
 
 
 available_food_names = ["суши", "спагетти", "хачапури"]
@@ -16,6 +17,7 @@ async def food_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for name in available_food_names:
         keyboard.add(name)
+    keyboard.add("Отмена")
     await message.answer("Выберите блюдо:", reply_markup=keyboard)
     await OrderFood.waiting_for_food_name.set()
 
@@ -24,11 +26,14 @@ async def food_chosen(message: types.Message, state: FSMContext):
     if message.text.lower() not in available_food_names:
         await message.answer("Пожалуйста, выберите блюдо, используя клавиатуру ниже.")
         return
+    if message.text.lower() == "Отмена":
+        cmd_cancel()
     await state.update_data(chosen_food=message.text.lower())
 
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for size in available_food_sizes:
         keyboard.add(size)
+    keyboard.add("Отмена")
     await OrderFood.next()
     await message.answer("Теперь выберите размер порции:", reply_markup=keyboard)
 
@@ -37,6 +42,8 @@ async def food_size_chosen(message: types.Message, state: FSMContext):
     if message.text.lower() not in available_food_sizes:
         await message.answer("Пожалуйста, выберите размер порции, используя клавиатуру ниже.")
         return
+    if message.text.lower() == "Отмена":
+        cmd_cancel()
     user_data = await state.get_data()
     await message.answer(f"Вы заказали {message.text.lower()} порцию {user_data['chosen_food']}.\n"
                          f"Поробуйте теперь заказать напитки: /drinks", reply_markup=types.ReplyKeyboardRemove())
