9308f525aa0fcd2eeaadadad9309a278284b01e7
Mikhail Ignatenko <Mixa418@gmail.com>
2021-12-15 15:52:32 +0300

    preparing a file of time handlers

--- Bot.py
+++ Bot.py
@@ -1,29 +1,18 @@
-import datetime
 import logging
 import asyncio
-import re
-from os import path
 
-import aioschedule
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils.callback_data import CallbackData
-from aiogram.types import BotCommand, callback_query
+from aiogram.types import BotCommand
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from app.config_reader import load_config
-from app.tgbot.auth import TUser
 from app.create_log import setup_logger
 
 from app.tgbot.administration.admin_handlers import register_handlers_admin
-
-from app.tgbot.administration.admin_handlers import get_keyboard_admin
+from app.tgbot.time_handlers import time_scanner
 from app.tgbot.users.stat_handlers import register_handlers_stat
 from app.tgbot.users.user_handlers import register_handlers_user, register_handlers_wait_input
 
-# config = load_config("config/bot.ini")
-# bot = Bot(token=config['tg_bot']['token'], parse_mode=types.ParseMode.HTML)
-# dp = Dispatcher(bot, storage=MemoryStorage())
 main_logger: logging.Logger = setup_logger("App.Bot.bot", "log/tgbot.log")
 bot_logger: logging.Logger = setup_logger("App.Bot", "log/bot.log")
 app_logger: logging.Logger = setup_logger("App", "log/app.log")
@@ -39,8 +28,7 @@ async def main_bot():
     bot = Bot(token=config['tg_bot']['token'], parse_mode=types.ParseMode.HTML)
     dp = Dispatcher(bot, storage=MemoryStorage())
 
-
-    # Регистрация хэндлеров
+    # Регистрация обработчиков
     register_handlers_admin(dp, bot, config['tg_bot']['admin_id'])
     register_handlers_user(dp)
     register_handlers_wait_input(dp, bot, config['tg_bot']['admin_id'])
@@ -53,54 +41,20 @@ async def main_bot():
     await dp.skip_updates()  # пропуск накопившихся апдейтов (необязательно)
     await dp.start_polling()
     main_logger.info("The bot has started")
-    # await executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
 
-# Регстрация команд, отображаемых в интерефейсе Телеграм
+# Регистрация команд, отображаемых в интерфейсе Телеграм
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(command="/menu", description="Взаимодействие с ботом"),
         BotCommand(command="/stat", description="Получить месячную статистику")
-
     ]
-    await bot.set_my_commands(commands)  # TODO наверх
-
-
-def log_in(*arg):
-    time = str(datetime.datetime.today())
-    string = time + ' ' + ' '.join([str(i) for i in arg])
-    with open('users_messages.txt', 'a', encoding='utf-8') as f:
-        print(string, file=f)
-
-
-
-async def noon_print():
-    five = [i for i in range(0, 60, 1)]
-    now_minute = datetime.datetime.now().minute
-    if now_minute in five:
-        print('Текущая минута - ', now_minute)
-
-
-async def print_second():
-    pass
-    # print(datetime.datetime.now().second)
-
-
-async def now_monday():
-    print('сегодня понедельник')
-
-async def on_startup():
-    aioschedule.every().minute.do(noon_print)
-    aioschedule.every(5).seconds.do(print_second)
-    aioschedule.every().monday.at("16:17").do(now_monday)
-    while True:
-        await aioschedule.run_pending()
-        await asyncio.sleep(1)
+    await bot.set_my_commands(commands)
 
 
 async def start():
     m = asyncio.create_task(main_bot())
-    t = asyncio.create_task(on_startup())
+    t = asyncio.create_task(time_scanner())
     await asyncio.gather(m, t)
 
 
---
+++ app/tgbot/time_handlers.py
@@ -0,0 +1,41 @@
+import datetime
+import logging
+import asyncio
+import aioschedule
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import BotCommand
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from app.create_log import setup_logger
+
+bot: Bot
+time_logger: logging.Logger = setup_logger("App.Bot.time", "log/time.log")
+
+
+def register_handlers_admin(dp: Dispatcher, main_bot: Bot, admin_id: int):
+    global bot
+    bot = main_bot
+
+
+async def noon_print():
+    now_hour = datetime.datetime.now().hour
+    print('Текущий час -', now_hour)
+
+
+async def print_second():
+    pass
+    # print(datetime.datetime.now().second)
+
+
+async def now_day():
+    now_date = datetime.datetime.now().day
+    print('Сегодняшнее число - %s' % now_date)
+
+
+async def time_scanner():
+    aioschedule.every(2).hour.do(noon_print)
+    aioschedule.every().day.at("00:01").do(now_day)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
